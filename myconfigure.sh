72af11a800f3e384ad29470336447d6d4f4b31b2
marxin <marxin.liska@gmail.com>
2013-07-24 23:09:00 +0200

    Original value profiles enabled.

--- myconfigure.sh
+++ myconfigure.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-CXXFLAGS="-ggdb3 -O0" CFLAGS="-ggdb3 -O0" ../configure --enable-languages=c,c++ --disable-bootstrap --disable-libsanitizer --prefix=/home/marxin/gcc-marxin
+PREFIX=/home/marxin/gcc-marxin
 
-# CXXFLAGS="-O0 -flto-partition=none -flto -fno-fat-lto-objects -fipa-sem-equality" CFLAGS="-O0 -flto-partition=none -flto -fno-fat-lto-objects -fipa-sem-equality" ../configure --enable-languages=c --disable-bootstrap --disable-libsanitizer --prefix=/home/marxin/gcc-test
-# ../configure --enable-languages=c --enable-bootstrap --disable-libsanitizer --prefix=/home/marxin/gcc-test
+CFLAGS="-ggdb3 -O0"
+# CFLAGS="-O2"
+
+CXXFLAGS=$CFLAGS CFLAGS=$CFLAGS ../configure --enable-languages=c,c++ --disable-bootstrap --prefix=$PREFIX
