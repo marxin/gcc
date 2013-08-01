46029a6f472ef4770864d9a48c5af9364a94b046
marxin <marxin.liska@gmail.com>
2013-08-01 18:20:18 +0200

    Scripts removed and documentation reverted.

--- myconfigure.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-PREFIX=/home/marxin/gcc-marxin
-
-#CFLAGS="-ggdb3 -O0"
-CFLAGS=""
-
-CXXFLAGS=$CFLAGS CFLAGS=$CFLAGS ../configure --enable-languages=c,c++ --disable-bootstrap --prefix=$PREFIX --enable-checking=release 
