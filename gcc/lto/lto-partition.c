6ca6baade7df780b925508849394dff4eb5b3a08
marxin <marxin.liska@gmail.com>
2013-08-11 00:38:35 +0200

    Dump functions fixed.

--- gcc/lto/lto-partition.c
+++ gcc/lto/lto-partition.c
@@ -496,14 +496,14 @@ lto_balanced_map (void)
   free (postorder);
 
 
-  if (dump_file && (dump_flags & TDF_DETAILS))
-    fprintf (dump_file, "Sorting by time profiler:\n");
+  if (cgraph_dump_file)
+    fprintf (cgraph_dump_file, "Sorting by time profiler:\n");
 
   qsort (order, n_nodes, sizeof (struct cgraph_node *), node_cmp);
 
-  if (dump_file && (dump_flags & TDF_DETAILS))
+  if (cgraph_dump_file)
     for(i = 0; i < n_nodes; i++)
-      fprintf (dump_file, "Balanced map symbol order:%s:%u\n", cgraph_node_asm_name (order[i]), order[i]->tp_first_run);
+      fprintf (cgraph_dump_file, "Balanced map symbol order:%s:%u\n", cgraph_node_asm_name (order[i]), order[i]->tp_first_run);
 
   if (!flag_toplevel_reorder)
     {
