8b0e4bba8f877680a97e1f8b829cf1883da3be73
mliska <mliska@suse.cz>
2014-07-17 14:54:03 +0200

    Suspicious new line removed.

--- gcc/ipa-utils.h
+++ gcc/ipa-utils.h
@@ -72,7 +72,6 @@ struct odr_type_d;
 typedef odr_type_d *odr_type;
 void build_type_inheritance_graph (void);
 void update_type_inheritance_graph (void);
-
 vec <cgraph_node *>
 possible_polymorphic_call_targets (tree, HOST_WIDE_INT,
 				   ipa_polymorphic_call_context,
