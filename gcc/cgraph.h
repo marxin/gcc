82cb8f03eb378c069a8a12257e16c1b455141b35
mliska <mliska@suse.cz>
2014-11-18 14:42:20 +0100

    New data structure for function_summary introduced.
    gcc/ChangeLog:
    
    2014-11-12  Martin Liska  <mliska@suse.cz>
    
    	* cgraph.h (symbol_table::allocate_cgraph_symbol): Summary UID
    	is filled up.
    	* symbol-summary.h: New file.
    	* gengtype.c (open_base_files): Add symbol-summary.h.
    	* toplev.c (general_init): Call constructor of symbol_table.

--- gcc/cgraph.h
+++ gcc/cgraph.h
@@ -1246,6 +1246,8 @@ public:
   int count_materialization_scale;
   /* Unique id of the node.  */
   int uid;
+  /* Summary unique id of the node.  */
+  int summary_uid;
   /* ID assigned by the profiling.  */
   unsigned int profile_id;
   /* Time profiler: first run of function.  */
@@ -1819,6 +1821,10 @@ public:
   friend class cgraph_node;
   friend class cgraph_edge;
 
+  symbol_table (): cgraph_max_summary_uid (1)
+  {
+  }
+
   /* Initialize callgraph dump file.  */
   void initialize (void);
 
@@ -2015,6 +2021,7 @@ public:
 
   int cgraph_count;
   int cgraph_max_uid;
+  int cgraph_max_summary_uid;
 
   int edges_count;
   int edges_max_uid;
@@ -2343,6 +2350,7 @@ symbol_table::allocate_cgraph_symbol (void)
       node->uid = cgraph_max_uid++;
     }
 
+  node->summary_uid = cgraph_max_summary_uid++;
   return node;
 }
 
