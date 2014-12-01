ff2b6e7843c384814e3b1f739d3ec1ccc9acda74
marxin <marxin.liska@gmail.com>
2014-12-01 13:57:39 +0100

    New working test added.

---
+++ hsa-tests/a.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+void __attribute__((hsa))
+square (int a, int x)
+{
+  int b = a + 4;
+  a += 55;
+}
+
+void __attribute__((hsakernel))
+foo (int *a, int *b, ...)
+{
+  int i = a[__builtin_omp_get_thread_num ()];
+  square (i, i);
+  b[__builtin_omp_get_thread_num ()] = i * i;
+  a[__builtin_omp_get_thread_num ()] = __builtin_omp_get_thread_num ();
+}
+
+#define N 16
+int a[N];
+int b[N];
+
+
+int
+main (int argc, char **argv)
+{
+  int i;
+  for (i = 4 ; i <= N; i += 4)
+    {
+      int j;
+      for (j = 0; j < N; j++)
+	a[j] = j;
+      __builtin_memset (b, 0, sizeof(int) * N);
+      foo (a, b, i, 4);
+      for (j = 0; j < N; j++)
+	fprintf (stdout, "%.3i ", a[j]);
+      fprintf (stdout, "\n");
+      for (j = 0; j < N; j++)
+	fprintf (stdout, "%.3i ", b[j]);
+      fprintf (stdout, "\n\n");
+    }
+  return 0;
+}
