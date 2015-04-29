e141c47556d7717e20d3ddb1f0d0bfd811a71508
mliska <mliska@suse.cz>
2015-04-29 17:26:31 +0200

    Change use to type-based pool allocator in ira-color.c.
    gcc/ChangeLog:
    
    2015-04-30  Martin Liska  <mliska@suse.cz>
    
    	* ira-color.c (init_update_cost_records):Use new type-based pool allocator.
    	(get_update_cost_record) Likewise.
    	(free_update_cost_record_list) Likewise.
    	(finish_update_cost_records) Likewise.
    	(initiate_cost_update) Likewise.

--- gcc/ira-color.c
+++ gcc/ira-color.c
@@ -1166,8 +1166,16 @@ setup_profitable_hard_regs (void)
    allocnos.  */
 
 /* Pool for update cost records.  */
-static pool_allocator<update_cost_record> update_cost_record_pool
-  ("update cost records", 100);
+static alloc_pool update_cost_record_pool;
+
+/* Initiate update cost records.  */
+static void
+init_update_cost_records (void)
+{
+  update_cost_record_pool
+    = create_alloc_pool ("update cost records",
+			 sizeof (struct update_cost_record), 100);
+}
 
 /* Return new update cost record with given params.  */
 static struct update_cost_record *
@@ -1176,7 +1184,7 @@ get_update_cost_record (int hard_regno, int divisor,
 {
   struct update_cost_record *record;
 
-  record = update_cost_record_pool.allocate ();
+  record = (struct update_cost_record *) pool_alloc (update_cost_record_pool);
   record->hard_regno = hard_regno;
   record->divisor = divisor;
   record->next = next;
@@ -1192,7 +1200,7 @@ free_update_cost_record_list (struct update_cost_record *list)
   while (list != NULL)
     {
       next = list->next;
-      update_cost_record_pool.remove (list);
+      pool_free (update_cost_record_pool, list);
       list = next;
     }
 }
@@ -1201,7 +1209,7 @@ free_update_cost_record_list (struct update_cost_record *list)
 static void
 finish_update_cost_records (void)
 {
-  update_cost_record_pool.release ();
+  free_alloc_pool (update_cost_record_pool);
 }
 
 /* Array whose element value is TRUE if the corresponding hard
@@ -1256,6 +1264,7 @@ initiate_cost_update (void)
     = (struct update_cost_queue_elem *) ira_allocate (size);
   memset (update_cost_queue_elems, 0, size);
   update_cost_check = 0;
+  init_update_cost_records ();
 }
 
 /* Deallocate data used by function update_costs_from_copies.  */
