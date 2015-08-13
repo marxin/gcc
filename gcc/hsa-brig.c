ffb0c3b190fcbb2c4799b70267f9708bcf8af9ea
mliska <mliska@suse.cz>
2015-08-13 11:48:02 +0200

    HSA: Implement memory copy operation.
    gcc/ChangeLog:
    
    2015-08-19  Martin Liska  <mliska@suse.cz>
    
        * hsa-gen.c (gen_hsa_addr): Add new sorry.
        (get_unsigned_type_by_bytes): New function.
        (gen_hsa_memory_copy): Newly reimplemented function.
        (gen_hsa_insns_for_single_assignment): Add support for assignments
        that can be done by memory copy operation.
        (gen_hsa_insns_for_direct_call): New sorry message.
        (get_unsinged_type_by_bytes): Removed.
        (gen_hsa_insns_for_kernel_call): Rename local variable.
        (gen_hsa_insns_for_call): Anothe sorry message.
        (gen_hsa_phi_from_gimple_phi): Support assignment of NULL value
        to a pointer type; support LSA instructions.
        * hsa.h: Fix indentation.

--- gcc/hsa-brig.c
+++ gcc/hsa-brig.c
@@ -793,6 +793,14 @@ emit_immediate_scalar_to_data_section (tree value, unsigned need_len)
   memset (&bytes, 0, sizeof (bytes));
   tree type = TREE_TYPE (value);
   gcc_checking_assert (TREE_CODE (type) != VECTOR_TYPE);
+
+  if (TREE_CODE (type) == REFERENCE_TYPE)
+    {
+      sorry ("Support for HSA does not implement immediate value "
+	     "of a reference type");
+      return 0;
+    }
+
   unsigned data_len = tree_to_uhwi (TYPE_SIZE (type))/BITS_PER_UNIT;
   if (INTEGRAL_TYPE_P (type))
     switch (data_len)
@@ -1679,9 +1687,8 @@ perhaps_emit_branch (basic_block bb, basic_block next_bb)
 	gcc_assert (!ff);
 	ff = e->dest;
       }
-  gcc_assert (ff);
-  if (ff == next_bb
-      || ff == EXIT_BLOCK_PTR_FOR_FN (cfun))
+
+  if (!ff || ff == next_bb || ff == EXIT_BLOCK_PTR_FOR_FN (cfun))
     return;
 
   repr.base.base.byteCount = htole16 (sizeof (repr));
