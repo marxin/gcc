3a90882c36429f94678fc5dba2952fe39bd495d4
mliska <mliska@suse.cz>
2015-08-19 09:26:05 +0200

    fixup! HSA: add support for assignment of NULL to a pointer.

--- gcc/hsa-brig.c
+++ gcc/hsa-brig.c
@@ -814,8 +814,7 @@ emit_immediate_scalar_to_data_section (tree value, unsigned need_len)
   gcc_checking_assert (TREE_CODE (type) != VECTOR_TYPE);
   unsigned data_len = tree_to_uhwi (TYPE_SIZE (type))/BITS_PER_UNIT;
   if (INTEGRAL_TYPE_P (type)
-      || (TREE_CODE (type) == POINTER_TYPE
-	  && TREE_CODE (TREE_TYPE (type)) == INTEGER_TYPE))
+      || (TREE_CODE (type) == POINTER_TYPE && TREE_CODE (value) == INTEGER_CST))
     switch (data_len)
       {
       case 1:
