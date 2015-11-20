70689c2f9476234877996fe23a99b56f8534284d
marxin <mliska@suse.cz>
2015-11-20 15:18:52 +0100

    Fix cilk memory leak in gimplification
    gcc/c-family/ChangeLog:
    
    2015-11-20  Martin Liska  <mliska@suse.cz>
    
    	* cilk.c (gimplify_cilk_spawn): Free allocated memory.

--- gcc/c-family/cilk.c
+++ gcc/c-family/cilk.c
@@ -844,6 +844,7 @@ gimplify_cilk_spawn (tree *spawn_p)
 			    call2, build_empty_stmt (EXPR_LOCATION (call1)));
   append_to_statement_list (spawn_expr, spawn_p);
 
+  free (arg_array);
   return GS_OK;
 }
 
