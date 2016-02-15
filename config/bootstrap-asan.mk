3bb1704712a8709addbf39050ef6daa4941f5171
marxin <mliska@suse.cz>
2016-02-15 16:07:04 +0100

    Add -fstack-reuse=none to bootstrap-asan.mk config file

--- config/bootstrap-asan.mk
+++ config/bootstrap-asan.mk
@@ -3,8 +3,8 @@
 # Suppress LeakSanitizer in bootstrap.
 export LSAN_OPTIONS="detect_leaks=0"
 
-STAGE2_CFLAGS += -fsanitize=address -fsanitize=use-after-scope
-STAGE3_CFLAGS += -fsanitize=address -fsanitize=use-after-scope
+STAGE2_CFLAGS += -fsanitize=address -fsanitize=use-after-scope -fstack-reuse=none
+STAGE3_CFLAGS += -fsanitize=address -fsanitize=use-after-scope -fstack-reuse=none
 POSTSTAGE1_LDFLAGS += -fsanitize=address -fsanitize=use-after-scope -static-libasan \
 		      -B$$r/prev-$(TARGET_SUBDIR)/libsanitizer/ \
 		      -B$$r/prev-$(TARGET_SUBDIR)/libsanitizer/asan/ \
