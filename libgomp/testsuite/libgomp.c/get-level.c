c844056ccc94cc1376a15db61db15376239a0e10
marxin <mliska@suse.cz>
2016-02-19 14:15:12 +0100

    HSA: add runtime-tests
    libgomp/ChangeLog:
    
    2016-02-19  Martin Liska  <mliska@suse.cz>
    
    	* testsuite/libgomp.c/bitfield-1.c: New test.
    	* testsuite/libgomp.c/cnt-incrementation.c: Likewise.
    	* testsuite/libgomp.c/complex-types.c: Likewise.
    	* testsuite/libgomp.c/function-call-1.c: Likewise.
    	* testsuite/libgomp.c/get-level.c: Likewise.
    	* testsuite/libgomp.c/hsa-alloca.c: Likewise.
    	* testsuite/libgomp.c/hsa-builtins-1.c: Likewise.
    	* testsuite/libgomp.c/hsa-formal-actual-args.c: Likewise.
    	* testsuite/libgomp.c/hsa-memory-operations.c: Likewise.
    	* testsuite/libgomp.c/rotate-1.c: Likewise.
    	* testsuite/libgomp.c/switch-1.c: Likewise.
    	* testsuite/libgomp.c/switch-branch-1.c: Likewise.

---
+++ libgomp/testsuite/libgomp.c/get-level.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <omp.h>
+
+int
+main ()
+{
+  int i;
+  int level = -1;
+
+#pragma omp target map(tofrom : level)
+  {
+    level = omp_get_level ();
+  }
+
+  if (level != 0)
+    abort ();
+
+#pragma omp target teams map(tofrom : level)
+#pragma omp distribute parallel for default(none) private(i) shared(level)
+  for (i = 0; i < 1; ++i)
+    level += omp_get_level ();
+
+  if (level != 1)
+    abort ();
+
+  return 0;
+}
