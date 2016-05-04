7456f7f75b5ba507172af0a517b5974124475f2e
marxin <mliska@suse.cz>
2016-05-04 12:57:05 +0200

    Introduce tests for -fsanitize=use-after-scope
    gcc/testsuite/ChangeLog:
    
    2016-05-10  Martin Liska  <mliska@suse.cz>
    
    	* g++.dg/asan/use-after-scope-1.C: New test.
    	* g++.dg/asan/use-after-scope-2.C: New test.
    	* gcc.dg/asan/use-after-scope-1.c: New test.
    	* gcc.dg/asan/use-after-scope-2.c: New test.
    	* gcc.dg/asan/use-after-scope-3.c: New test.
    	* gcc.dg/asan/use-after-scope-4.c: New test.
    	* gcc.dg/asan/use-after-scope-5.c: New test.
    	* gcc.dg/asan/use-after-scope-goto-1.c: New test.

---
+++ gcc/testsuite/gcc.dg/asan/use-after-scope-1.c
@@ -0,0 +1,19 @@
+// { dg-do run }
+// { dg-additional-options "-fsanitize-address-use-after-scope" }
+// { dg-shouldfail "asan" }
+
+int
+main (void)
+{
+  char *ptr;
+  {
+    char my_char[9];
+    ptr = &my_char[0];
+  }
+
+  return *(ptr+8);
+}
+
+// { dg-output "ERROR: AddressSanitizer: stack-use-after-scope on address.*(\n|\r\n|\r)" }
+// { dg-output "READ of size 1 at.*" }
+// { dg-output ".*'my_char' <== Memory access at offset \[0-9\]* is inside this variable.*" }
---
+++ gcc/testsuite/gcc.dg/asan/use-after-scope-4.c
@@ -0,0 +1,17 @@
+// { dg-do run }
+// { dg-additional-options "-fsanitize-address-use-after-scope" }
+
+int
+__attribute__((no_sanitize_address))
+main (void)
+{
+  char *ptr;
+  char *ptr2;
+  {
+    char my_char[9];
+    ptr = &my_char[0];
+    __builtin_memcpy (&ptr2, &ptr, sizeof (ptr2));
+  }
+
+  *(ptr2+9) = 'c';
+}
