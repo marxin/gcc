53dd3c035283863a25a24feb90bf359295999bca
marxin <mliska@suse.cz>
2016-11-11 11:21:29 +0100

    Fix PR sanitizer/78270 (part 2)
    gcc/ChangeLog:
    
    2016-11-11  Martin Liska  <mliska@suse.cz>
    
    	PR sanitizer/78270
    	* gimplify.c (gimplify_switch_expr): Always save previous
    	gimplify_ctxp->live_switch_vars.
    
    gcc/testsuite/ChangeLog:
    
    2016-11-11  Martin Liska  <mliska@suse.cz>
    
    	PR sanitizer/78270
    	* gcc.dg/asan/pr78270-2.c: New test.

---
+++ gcc/testsuite/gcc.dg/asan/pr78270-2.c
@@ -0,0 +1,17 @@
+// { dg-do compile }
+// { dg-additional-options "-Wno-switch-unreachable" }
+
+int a;
+void
+fn1 ()
+{
+  switch (a)
+    {
+      char b;
+    case 8:
+      &b;
+      switch (0)
+	;
+    }
+}
+
