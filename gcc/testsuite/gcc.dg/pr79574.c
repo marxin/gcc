c92bbded326bb117d6c5bfcb1f505f2bbffc0b75
marxin <mliska@suse.cz>
2017-02-17 13:46:21 +0100

    Use HOST_WIDE_INT for a param calculation (PR rtl-optimization/79574).
    gcc/testsuite/ChangeLog:
    
    2017-02-17  Martin Liska  <mliska@suse.cz>
    
    	PR rtl-optimization/79574
    	* gcc.dg/pr79574.c: New test.
    
    gcc/ChangeLog:
    
    2017-02-17  Martin Liska  <mliska@suse.cz>
    
    	PR rtl-optimization/79574
    	* gcse.c (want_to_gcse_p): Prevent integer overflow.

---
+++ gcc/testsuite/gcc.dg/pr79574.c
@@ -0,0 +1,11 @@
+/* PR rtl-optimization/79080 */
+/* { dg-do compile } */
+/* { dg-options "-Os --param gcse-cost-distance-ratio=2147483647" } */
+
+
+void a (void)
+{
+  volatile int b;
+  for (;; b)
+    ;
+}
