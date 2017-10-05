6da7d154c494b6d1dccbbe350467bb0b8e8d183f
marxin <mliska@suse.cz>
2017-10-05 12:14:25 +0200

    Add -fsanitize=pointer-{compare,subtract}.
    gcc/ChangeLog:
    
    2017-10-13  Martin Liska  <mliska@suse.cz>
    
    	* doc/invoke.texi: Document the options.
    	* flag-types.h (enum sanitize_code): Add
    	SANITIZE_POINTER_COMPARE and SANITIZE_POINTER_SUBTRACT.
    	* ipa-inline.c (sanitize_attrs_match_for_inline_p): Add handling
    	of SANITIZE_POINTER_COMPARE and SANITIZE_POINTER_SUBTRACT.
    	* opts.c: Define new sanitizer options.
    	* sanitizer.def (BUILT_IN_ASAN_POINTER_COMPARE): Likewise.
    	(BUILT_IN_ASAN_POINTER_SUBTRACT): Likewise.
    
    gcc/c/ChangeLog:
    
    2017-10-13  Martin Liska  <mliska@suse.cz>
    
    	* c-typeck.c (pointer_diff): Add new argument and instrument
    	pointer subtraction.
    	(build_binary_op): Similar for pointer comparison.
    
    gcc/cp/ChangeLog:
    
    2017-10-16  Martin Liska  <mliska@suse.cz>
    
    	* typeck.c (pointer_diff): Add new argument and instrument
    	pointer subtraction.
    	(cp_build_binary_op): Create compound expression if doing an
    	instrumentation.
    
    gcc/testsuite/ChangeLog:
    
    2017-10-16  Martin Liska  <mliska@suse.cz>
    
    	* c-c++-common/asan/pointer-compare-1.c: New test.
    	* c-c++-common/asan/pointer-compare-2.c: New test.
    	* c-c++-common/asan/pointer-subtract-1.c: New test.
    	* c-c++-common/asan/pointer-subtract-2.c: New test.

--- libsanitizer/asan/asan_descriptions.cc
+++ libsanitizer/asan/asan_descriptions.cc
@@ -220,6 +220,16 @@ bool GetStackAddressInformation(uptr addr, uptr access_size,
   return true;
 }
 
+bool GetStackVariableBeginning(uptr addr, uptr *shadow_addr)
+{
+  if (AsanThread *t = FindThreadByStackAddress(addr)) {
+    *shadow_addr = t->GetStackFrameVariableBeginning(addr);
+    return true;
+  }
+  else
+    return false;
+}
+
 static void PrintAccessAndVarIntersection(const StackVarDescr &var, uptr addr,
                                           uptr access_size, uptr prev_var_end,
                                           uptr next_var_beg) {
@@ -328,6 +338,26 @@ void GlobalAddressDescription::Print(const char *bug_type) const {
       StackDepotGet(reg_sites[i]).Print();
     }
   }
+}
+
+bool GlobalAddressDescription::PointsInsideASameVariable
+  (const GlobalAddressDescription &other) const
+{
+  if (size == 0 || other.size == 0)
+    return false;
+
+  for (int i = 0; i < size; i++)
+    for (int j = 0; j < other.size; j++) {
+      if (globals[i].beg == other.globals[j].beg &&
+	  globals[i].beg <= addr &&
+	  other.globals[j].beg <= other.addr &&
+	  (addr + access_size) < (globals[i].beg + globals[i].size) &&
+	  ((other.addr + other.access_size)
+	   < (other.globals[j].beg + other.globals[j].size)))
+	return true;
+    }
+
+  return false;
 }
 
 void StackAddressDescription::Print() const {
