361fa3cf17d5f579feb4d9f5c1625b1f44b3b595
marxin <mliska@suse.cz>
2018-01-18 15:54:08 +0100

    GCC 4.3: remove tests that fail and remove x32 target selector.

--- gcc/testsuite/gcc.target/i386/ret-thunk-18.c
+++ gcc/testsuite/gcc.target/i386/ret-thunk-18.c
@@ -1,6 +1,5 @@
 /* { dg-do compile { target { lp64 } } } */
 /* { dg-options "-O2 -mfunction-return=thunk-extern -mindirect-branch=keep -mcmodel=large" } */
-/* { dg-additional-options "-fPIC" { target fpic } } */
 
 void
 bar (void)
