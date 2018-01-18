9c9f10b6e92c0d363968868ee9be5fc274ec100c
marxin <mliska@suse.cz>
2018-01-18 15:54:08 +0100

    GCC 4.3: remove tests that fail and remove x32 target selector.

--- gcc/testsuite/gcc.target/i386/indirect-thunk-attr-10.c
+++ gcc/testsuite/gcc.target/i386/indirect-thunk-attr-10.c
@@ -1,6 +1,5 @@
 /* { dg-do compile { target { lp64 } } } */
 /* { dg-options "-O2 -mindirect-branch=keep -mfunction-return=keep -mcmodel=large" } */
-/* { dg-additional-options "-fPIC" { target fpic } } */
 
 __attribute__ ((indirect_branch("thunk-extern")))
 void
