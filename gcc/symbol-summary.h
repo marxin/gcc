dcb6ab883dba3aa44e634ea39de448b015cc3d2c
marxin <mliska@suse.cz>
2019-02-12 10:31:34 +0100

    Come up with function_summary_base class.

--- gcc/symbol-summary.h
+++ gcc/symbol-summary.h
@@ -21,6 +21,78 @@ along with GCC; see the file COPYING3.  If not see
 #ifndef GCC_SYMBOL_SUMMARY_H
 #define GCC_SYMBOL_SUMMARY_H
 
+/* Base class for function_summary and function_vector_summary classes.  */
+
+template <class T>
+class function_summary_base
+{
+public:
+  /* Default construction takes SYMTAB as an argument.  */
+  function_summary_base (symbol_table *symtab): m_symtab (symtab),
+  m_insertion_enabled (true), m_released (false)
+  {}
+
+  /* Basic implementation of insert operation.  */
+  virtual void insert (cgraph_node *, T *) {}
+
+  /* Basic implementation of removal operation.  */
+  virtual void remove (cgraph_node *, T *) {}
+
+  /* Basic implementation of duplication operation.  */
+  virtual void duplicate (cgraph_node *, cgraph_node *, T *, T *) {}
+
+  /* Enable insertion hook invocation.  */
+  void enable_insertion_hook ()
+  {
+    m_insertion_enabled = true;
+  }
+
+  /* Enable insertion hook invocation.  */
+  void disable_insertion_hook ()
+  {
+    m_insertion_enabled = false;
+  }
+
+protected:
+  /* Allocates new data that are stored within map.  */
+  T* allocate_new ()
+  {
+    /* Call gcc_internal_because we do not want to call finalizer for
+       a type T.  We call dtor explicitly.  */
+    return is_ggc () ? new (ggc_internal_alloc (sizeof (T))) T () : new T () ;
+  }
+
+  /* Release an item that is stored within map.  */
+  void release (T *item)
+  {
+    if (is_ggc ())
+      {
+	item->~T ();
+	ggc_free (item);
+      }
+    else
+      delete item;
+  }
+
+  /* Internal summary insertion hook pointer.  */
+  cgraph_node_hook_list *m_symtab_insertion_hook;
+  /* Internal summary removal hook pointer.  */
+  cgraph_node_hook_list *m_symtab_removal_hook;
+  /* Internal summary duplication hook pointer.  */
+  cgraph_2node_hook_list *m_symtab_duplication_hook;
+  /* Symbol table the summary is registered to.  */
+  symbol_table *m_symtab;
+
+  /* Indicates if insertion hook is enabled.  */
+  bool m_insertion_enabled;
+  /* Indicates if the summary is released.  */
+  bool m_released;
+
+private:
+  /* Return true when the summary uses GGC memory for allocation.  */
+  virtual bool is_ggc () = 0;
+};
+
 /* We want to pass just pointer types as argument for function_summary
    template class.  */
 
@@ -43,7 +115,7 @@ private:
    a memory gained by garbage collected memory.  */
 
 template <class T>
-class GTY((user)) function_summary <T *>
+class GTY((user)) function_summary <T *>: public function_summary_base<T>
 {
 public:
   /* Default construction takes SYMTAB as an argument.  */
@@ -55,7 +127,8 @@ public:
     release ();
   }
 
-  /* Destruction method that can be called for GGT purpose.  */
+  /* Destruction method that can be called for GGC purpose.  */
+  using function_summary_base<T>::release;
   void release ();
 
   /* Traverses all summarys with a function F called with
@@ -66,26 +139,6 @@ public:
     m_map.traverse <f> (a);
   }
 
-  /* Basic implementation of insert operation.  */
-  virtual void insert (cgraph_node *, T *) {}
-
-  /* Basic implementation of removal operation.  */
-  virtual void remove (cgraph_node *, T *) {}
-
-  /* Basic implementation of duplication operation.  */
-  virtual void duplicate (cgraph_node *, cgraph_node *, T *, T *) {}
-
-  /* Allocates new data that are stored within map.  */
-  T* allocate_new ()
-  {
-    /* Call gcc_internal_because we do not want to call finalizer for
-       a type T.  We call dtor explicitly.  */
-    return m_ggc ? new (ggc_internal_alloc (sizeof (T))) T () : new T () ;
-  }
-
-  /* Release an item that is stored within map.  */
-  void release (T *item);
-
   /* Getter for summary callgraph node pointer.  If a summary for a node
      does not exist it will be created.  */
   T* get_create (cgraph_node *node)
@@ -93,7 +146,7 @@ public:
     bool existed;
     T **v = &m_map.get_or_insert (node->get_uid (), &existed);
     if (!existed)
-      *v = allocate_new ();
+      *v = this->allocate_new ();
 
     return *v;
   }
@@ -106,6 +159,7 @@ public:
   }
 
   /* Remove node from summary.  */
+  using function_summary_base<T>::remove;
   void remove (cgraph_node *node)
   {
     int uid = node->get_uid ();
@@ -113,7 +167,7 @@ public:
     if (v)
       {
 	m_map.remove (uid);
-	release (*v);
+	this->release (*v);
       }
   }
 
@@ -121,18 +175,6 @@ public:
   bool exists (cgraph_node *node)
   {
     return m_map.get (node->get_uid ()) != NULL;
-  }
-
-  /* Enable insertion hook invocation.  */
-  void enable_insertion_hook ()
-  {
-    m_insertion_enabled = true;
-  }
-
-  /* Enable insertion hook invocation.  */
-  void disable_insertion_hook ()
-  {
-    m_insertion_enabled = false;
   }
 
   /* Symbol insertion hook that is registered to symbol table.  */
@@ -152,20 +194,14 @@ protected:
 private:
   typedef int_hash <int, 0, -1> map_hash;
 
-  /* Indicates if insertion hook is enabled.  */
-  bool m_insertion_enabled;
-  /* Indicates if the summary is released.  */
-  bool m_released;
+  /* Indication if we use ggc summary.  */
+  virtual bool is_ggc ()
+  {
+    return m_ggc;
+  }
+
   /* Main summary store, where summary ID is used as key.  */
   hash_map <map_hash, T *> m_map;
-  /* Internal summary insertion hook pointer.  */
-  cgraph_node_hook_list *m_symtab_insertion_hook;
-  /* Internal summary removal hook pointer.  */
-  cgraph_node_hook_list *m_symtab_removal_hook;
-  /* Internal summary duplication hook pointer.  */
-  cgraph_2node_hook_list *m_symtab_duplication_hook;
-  /* Symbol table the summary is registered to.  */
-  symbol_table *m_symtab;
 
   template <typename U> friend void gt_ggc_mx (function_summary <U *> * const &);
   template <typename U> friend void gt_pch_nx (function_summary <U *> * const &);
@@ -175,50 +211,35 @@ private:
 
 template <typename T>
 function_summary<T *>::function_summary (symbol_table *symtab, bool ggc):
-  m_ggc (ggc), m_insertion_enabled (true), m_released (false), m_map (13, ggc),
-  m_symtab (symtab)
+  function_summary_base<T> (symtab), m_ggc (ggc), m_map (13, ggc)
 {
-  m_symtab_insertion_hook
-    = symtab->add_cgraph_insertion_hook (function_summary::symtab_insertion,
-					 this);
-
-  m_symtab_removal_hook
-    = symtab->add_cgraph_removal_hook (function_summary::symtab_removal, this);
-  m_symtab_duplication_hook
-    = symtab->add_cgraph_duplication_hook (function_summary::symtab_duplication,
-					   this);
+  this->m_symtab_insertion_hook
+    = this->m_symtab->add_cgraph_insertion_hook (function_summary::symtab_insertion,
+						 this);
+  this->m_symtab_removal_hook
+    = this->m_symtab->add_cgraph_removal_hook (function_summary::symtab_removal, this);
+  this->m_symtab_duplication_hook
+    = this->m_symtab->add_cgraph_duplication_hook (function_summary::symtab_duplication,
+						   this);
 }
 
 template <typename T>
 void
 function_summary<T *>::release ()
 {
-  if (m_released)
+  if (this->m_released)
     return;
 
-  m_symtab->remove_cgraph_insertion_hook (m_symtab_insertion_hook);
-  m_symtab->remove_cgraph_removal_hook (m_symtab_removal_hook);
-  m_symtab->remove_cgraph_duplication_hook (m_symtab_duplication_hook);
+  this->m_symtab->remove_cgraph_insertion_hook (this->m_symtab_insertion_hook);
+  this->m_symtab->remove_cgraph_removal_hook (this->m_symtab_removal_hook);
+  this->m_symtab->remove_cgraph_duplication_hook (this->m_symtab_duplication_hook);
 
   /* Release all summaries.  */
   typedef typename hash_map <map_hash, T *>::iterator map_iterator;
   for (map_iterator it = m_map.begin (); it != m_map.end (); ++it)
-    release ((*it).second);
+    this->release ((*it).second);
 
-  m_released = true;
-}
-
-template <typename T>
-void
-function_summary<T *>::release (T *item)
-{
-  if (m_ggc)
-    {
-      item->~T ();
-      ggc_free (item);
-    }
-  else
-    delete item;
+  this->m_released = true;
 }
 
 template <typename T>
@@ -278,6 +299,8 @@ gt_pch_nx(function_summary<T *>* const& summary, gt_pointer_operator op,
   gt_pch_nx (&summary->m_map, op, cookie);
 }
 
+/* Help template from std c++11.  */
+
 template<typename T, typename U>
 struct is_same 
 {
@@ -290,7 +313,7 @@ struct is_same<T,T>  //specialization
    static const bool value = true; 
 };
 
-/* We want to pass just pointer types as argument for function_summary
+/* We want to pass just pointer types as argument for function_vector_summary
    template class.  */
 
 template <class T, class V>
@@ -300,16 +323,8 @@ private:
   function_vector_summary();
 };
 
-/* Function summary is a helper class that is used to associate a data structure
-   related to a callgraph node.  Typical usage can be seen in IPA passes which
-   create a temporary pass-related structures.  The summary class registers
-   hooks that are triggered when a new node is inserted, duplicated and deleted.
-   A user of a summary class can ovewrite virtual methods than are triggered by
-   the summary if such hook is triggered.  Apart from a callgraph node, the user
-   is given a data structure tied to the node.
-
-   The function summary class can work both with a heap-allocated memory and
-   a memory gained by garbage collected memory.  */
+/* Function vector summary is a fast implementation of function_summary that
+   utilizes vector as primary storage of summaries.  */
 
 template <class T, class V>
 class GTY((user)) function_vector_summary <T *, V>
@@ -324,7 +339,7 @@ public:
     release ();
   }
 
-  /* Destruction method that can be called for GGT purpose.  */
+  /* Destruction method that can be called for GGC purpose.  */
   void release ();
 
   /* Traverses all summarys with a function F called with
