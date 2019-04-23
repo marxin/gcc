94b606e62ccf1e673e5572aa2e156868cb2ff6cc
marxin <mliska@suse.cz>
2019-04-23 11:55:19 +0200

    Fix copyright in .c and .h files.

--- gcc/config/i386/i386-features.c
+++ gcc/config/i386/i386-features.c
@@ -1,5 +1,4 @@
-/* Subroutines used for code generation on IA-32.
-   Copyright (C) 2019 Free Software Foundation, Inc.
+/* Copyright (C) 1988-2019 Free Software Foundation, Inc.
 
 This file is part of GCC.
 
