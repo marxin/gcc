8a0bdb959ea6b3f45f2422d35a90c60733716448
Martin Liska <mliska@suse.cz>
2019-05-31 12:57:01 +0200

    Remove compare_memory_operand and use compare_operand.

--- gcc/ipa-icf-gimple.h
+++ gcc/ipa-icf-gimple.h
@@ -203,9 +203,6 @@ public:
   /* Verifies that tree labels T1 and T2 correspond.  */
   bool compare_tree_ssa_label (tree t1, tree t2);
 
-  /* Function compare for equality given memory operands T1 and T2.  */
-  bool compare_memory_operand (tree t1, tree t2);
-
   /* Function responsible for comparison of various operands T1 and T2.
      If these components, from functions FUNC1 and FUNC2, are equal, true
      is returned.  */
