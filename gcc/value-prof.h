516b2a906f0a8c428bff5364f34f4dfdd135d5ee
Martin Liska <mliska@suse.cz>
2019-06-06 10:17:13 +0200

    Implement N disk counters for single value and indirect call counters.
    gcc/ChangeLog:
    
    2019-06-04  Martin Liska  <mliska@suse.cz>
    
    	* gcov-io.h (GCOV_DISK_SINGLE_VALUES): New.
    	(GCOV_SINGLE_VALUE_COUNTERS): Likewise.
    	* ipa-profile.c (ipa_profile_generate_summary):
    	Use get_most_common_single_value.
    	* tree-profile.c (gimple_init_gcov_profiler):
    	Instrument with __gcov_one_value_profiler_v2
    	and __gcov_indirect_call_profiler_v4.
    	* value-prof.c (dump_histogram_value):
    	Print all values for HIST_TYPE_SINGLE_VALUE.
    	(stream_in_histogram_value): Set number of
    	counters for HIST_TYPE_SINGLE_VALUE.
    	(get_most_common_single_value): New.
    	(gimple_divmod_fixed_value_transform):
    	Use get_most_common_single_value.
    	(gimple_ic_transform): Likewise.
    	(gimple_stringops_transform): Likewise.
    	(gimple_find_values_to_profile): Set number
    	of counters for HIST_TYPE_SINGLE_VALUE.
    	* value-prof.h (get_most_common_single_value):
    	New.
    
    libgcc/ChangeLog:
    
    2019-06-04  Martin Liska  <mliska@suse.cz>
    
    	* Makefile.in: Add __gcov_one_value_profiler_v2 and
    	__gcov_indirect_call_profiler_v4.
    	* libgcov-merge.c (__gcov_merge_single): Change
    	function signature.
    	(merge_single_value_set): New.
    	* libgcov-profiler.c (__gcov_one_value_profiler_body):
    	Do not update counters[2].
    	(__gcov_one_value_profiler): Remove.
    	(__gcov_one_value_profiler_atomic): Rename to ...
    	(__gcov_one_value_profiler_v2): ... this.
    	(__gcov_indirect_call_profiler_v3): Rename to ...
    	(__gcov_indirect_call_profiler_v4): ... this.
    	* libgcov.h (__gcov_one_value_profiler): Remove.
    	(__gcov_one_value_profiler_atomic): Remove.
    	(__gcov_indirect_call_profiler_v3): Remove.
    	(__gcov_one_value_profiler_v2): New.
    	(__gcov_indirect_call_profiler_v4): New.

--- gcc/value-prof.h
+++ gcc/value-prof.h
@@ -90,6 +90,8 @@ void free_histograms (function *);
 void stringop_block_profile (gimple *, unsigned int *, HOST_WIDE_INT *);
 gcall *gimple_ic (gcall *, struct cgraph_node *, profile_probability);
 bool check_ic_target (gcall *, struct cgraph_node *);
+bool get_most_common_single_value (histogram_value hist,
+				   gcov_type *value, gcov_type *count);
 
 
 /* In tree-profile.c.  */
