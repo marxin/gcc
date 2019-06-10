7a125dda0250fd05fcb87d7d36df69b900d73b2f
Martin Liska <mliska@suse.cz>
2019-06-10 13:05:58 +0200

    operand_equal_p: add support for FIELD_DECL

--- gcc/fold-const.c
+++ gcc/fold-const.c
@@ -3462,11 +3462,35 @@ operand_equal_p (const_tree arg0, const_tree arg1, unsigned int flags)
 	}
 
     case tcc_declaration:
-      /* Consider __builtin_sqrt equal to sqrt.  */
-      return (TREE_CODE (arg0) == FUNCTION_DECL
-	      && fndecl_built_in_p (arg0) && fndecl_built_in_p (arg1)
-	      && DECL_BUILT_IN_CLASS (arg0) == DECL_BUILT_IN_CLASS (arg1)
-	      && DECL_FUNCTION_CODE (arg0) == DECL_FUNCTION_CODE (arg1));
+      switch (TREE_CODE (arg0))
+	{
+	case FUNCTION_DECL:
+	  /* Consider __builtin_sqrt equal to sqrt.  */
+	  return (fndecl_built_in_p (arg0) && fndecl_built_in_p (arg1)
+		  && DECL_BUILT_IN_CLASS (arg0) == DECL_BUILT_IN_CLASS (arg1)
+		  && DECL_FUNCTION_CODE (arg0) == DECL_FUNCTION_CODE (arg1));
+	case FIELD_DECL:
+	  {
+	    tree fo0 = DECL_FIELD_OFFSET (arg0);
+	    tree fo1 = DECL_FIELD_OFFSET (arg1);
+	    if (fo0 != NULL && fo1 != NULL
+		&& !operand_equal_p (fo0, fo1, OEP_ONLY_CONST))
+	      return false;
+	    else if (fo0 != fo1)
+	      return false;
+
+	    tree fbo0 = DECL_FIELD_BIT_OFFSET (arg0);
+	    tree fbo1 = DECL_FIELD_BIT_OFFSET (arg1);
+	    if (fbo0 != NULL && fbo1 != NULL
+		&& !operand_equal_p (fbo0, fbo1, OEP_ONLY_CONST))
+	      return false;
+	    else if (fbo0 != fbo1)
+	      return false;
+	    return true;
+	  }
+	default:
+	  return false;
+	}
 
     case tcc_exceptional:
       if (TREE_CODE (arg0) == CONSTRUCTOR)
