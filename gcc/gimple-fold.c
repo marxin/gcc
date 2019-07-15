8b02da2ff11c6017ee9d61628bd03cc15c703814
Li Jia He <helijia@linux.ibm.com>
2019-07-15 00:30:25 -0500

    Auto-generate maybe_fold_and/or_comparisons from match.pd
    Hi,
    
    This patch try to auto-generate maybe_fold_and/or_comparisons from match.pd.
    We try to build tempory assign with SSA def and then feed that into the GIMPLE
    match.pd path.
    
    In order to lower the cost of building SSA names / gimple stmts significantly,
    we need to allocated them on the stack.  This will cause the code a bit ugly.
    
    The regression testing for the patch was done on GCC mainline on
    
    	powerpc64le-unknown-linux-gnu (Power 9 LE)
    
    with no regressions.  Is it OK for trunk ?
    
    Thanks,
    Lijia He
    
    gcc/ChangeLog
    
    2019-07-16  Li Jia He  <helijia@linux.ibm.com>
    
    	* gimple.h (gimple_set_code): Declare.
    	(gimple_size): Likewise.
    	* gimple.c (gimple_set_code): Remove static and inline restrictions.
    	(gimple_size): Likewise.
    	* gimple-fold.c (maybe_fold_comparisons_from_match_pd): New function.
    	(maybe_fold_and_comparisons): Modify and_comparisons_1 invocation and
    	call maybe_fold_comparisons_from_match_pd.
    	(maybe_fold_or_comparisons): Modify or_comparisons_1 invocation and
    	call maybe_fold_comparisons_from_match_pd.

--- gcc/gimple-fold.c
+++ gcc/gimple-fold.c
@@ -5834,6 +5834,91 @@ and_comparisons_1 (enum tree_code code1, tree op1a, tree op1b,
   return NULL_TREE;
 }
 
+/* Helper function for maybe_fold_and_comparisons and maybe_fold_or_comparisons
+   : try to simplify the AND/OR of the ssa variable VAR with the comparison
+   specified by (OP2A CODE2 OP2B) from match.pd.  Return NULL_EXPR if we can't
+   simplify this to a single expression.  As we are going to lower the cost
+   of building SSA names / gimple stmts significantly, we need to allocate
+   them ont the stack.  This will cause the code to be a bit ugly.  */
+
+static tree
+maybe_fold_comparisons_from_match_pd (enum tree_code code, enum tree_code code1,
+				      tree op1a, tree op1b,
+				      enum tree_code code2, tree op2a,
+				      tree op2b)
+{
+  /* Allocate gimple stmt1 on the stack.  */
+  gimple *stmt1 = (gimple *) (XALLOCAVEC (char, gimple_size (GIMPLE_ASSIGN)
+						  + 2 * sizeof (tree)));
+  memset (stmt1, 0, gimple_size (GIMPLE_ASSIGN) + 2 * sizeof (tree));
+  gimple_set_code (stmt1, GIMPLE_ASSIGN);
+  gimple_set_num_ops (stmt1, 3);
+  gimple_init_singleton (stmt1);
+
+  /* Allocate gimple stmt2 on the stack.  */
+  gimple *stmt2 = (gimple *) (XALLOCAVEC (char, gimple_size (GIMPLE_ASSIGN)
+						  + 2 * sizeof (tree)));
+  memset (stmt2, 0, gimple_size (GIMPLE_ASSIGN) + 2 * sizeof (tree));
+  gimple_set_code (stmt2, GIMPLE_ASSIGN);
+  gimple_set_num_ops (stmt2, 3);
+  gimple_init_singleton (stmt2);
+
+  gimple_assign_set_rhs_code (stmt1, code1);
+  gimple_assign_set_rhs1 (stmt1, op1a);
+  gimple_assign_set_rhs2 (stmt1, op1b);
+
+  gimple_assign_set_rhs_code (stmt2, code2);
+  gimple_assign_set_rhs1 (stmt2, op2a);
+  gimple_assign_set_rhs2 (stmt2, op2b);
+
+  /* Allocate SSA names(lhs1) on the stack.  */
+  tree lhs1 = XALLOCA (tree_node);
+  memset (lhs1, 0, sizeof (tree_node));
+  TREE_SET_CODE (lhs1, SSA_NAME);
+  TREE_TYPE (lhs1) = boolean_type_node;
+
+  /* Allocate SSA names(lhs2) on the stack.  */
+  tree lhs2 = XALLOCA (tree_node);
+  memset (lhs2, 0, sizeof (tree_node));
+  TREE_SET_CODE (lhs2, SSA_NAME);
+  TREE_TYPE (lhs2) = boolean_type_node;
+
+  gimple_assign_set_lhs (stmt1, lhs1);
+  gimple_assign_set_lhs (stmt2, lhs2);
+
+  /* Call the interface function of match.pd to simplify the expression.  */
+  tree t = gimple_simplify (code, boolean_type_node, gimple_assign_lhs (stmt1),
+			    gimple_assign_lhs (stmt2), NULL,
+			    follow_all_ssa_edges);
+
+  if (t)
+    {
+      switch (TREE_CODE (t))
+	{
+	  case SSA_NAME:
+	    {
+	      gimple *def = SSA_NAME_DEF_STMT (t);
+
+	      if (!is_gimple_assign (def)
+		  || TREE_CODE_CLASS (gimple_assign_rhs_code (def))
+		    != tcc_comparison)
+		return NULL_TREE;
+
+	      return fold_build2 (gimple_assign_rhs_code (def),
+				  boolean_type_node, gimple_assign_rhs1 (def),
+				  gimple_assign_rhs2 (def));
+	  }
+	case INTEGER_CST:
+	  /* Fold expression to boolean_true_node or boolean_false_node.  */
+	  return t;
+	default:
+	  return NULL_TREE;
+	}
+    }
+
+  return NULL_TREE;
+}
+
 /* Try to simplify the AND of two comparisons, specified by
    (OP1A CODE1 OP1B) and (OP2B CODE2 OP2B), respectively.
    If this can be simplified to a single expression (without requiring
@@ -5845,11 +5930,21 @@ tree
 maybe_fold_and_comparisons (enum tree_code code1, tree op1a, tree op1b,
 			    enum tree_code code2, tree op2a, tree op2b)
 {
-  tree t = and_comparisons_1 (code1, op1a, op1b, code2, op2a, op2b);
-  if (t)
+  if (tree t = and_comparisons_1 (code1, op1a, op1b, code2, op2a, op2b))
     return t;
-  else
-    return and_comparisons_1 (code2, op2a, op2b, code1, op1a, op1b);
+
+  if (tree t = and_comparisons_1 (code2, op2a, op2b, code1, op1a, op1b))
+    return t;
+
+  if (tree t = maybe_fold_comparisons_from_match_pd (BIT_AND_EXPR, code1, op1a,
+						     op1b, code2, op2a, op2b))
+    return t;
+
+  if (tree t = maybe_fold_comparisons_from_match_pd (BIT_AND_EXPR, code2, op2a,
+						     op2b, code1, op1a, op1b))
+    return t;
+
+  return NULL_TREE;
 }
 
 /* Helper function for or_comparisons_1:  try to simplify the OR of the
@@ -6309,13 +6404,22 @@ tree
 maybe_fold_or_comparisons (enum tree_code code1, tree op1a, tree op1b,
 			   enum tree_code code2, tree op2a, tree op2b)
 {
-  tree t = or_comparisons_1 (code1, op1a, op1b, code2, op2a, op2b);
-  if (t)
+  if (tree t = or_comparisons_1 (code1, op1a, op1b, code2, op2a, op2b))
     return t;
-  else
-    return or_comparisons_1 (code2, op2a, op2b, code1, op1a, op1b);
-}
 
+  if (tree t = or_comparisons_1 (code2, op2a, op2b, code1, op1a, op1b))
+    return t;
+
+  if (tree t = maybe_fold_comparisons_from_match_pd (BIT_IOR_EXPR, code1, op1a,
+						     op1b, code2, op2a, op2b))
+    return t;
+
+  if (tree t = maybe_fold_comparisons_from_match_pd (BIT_IOR_EXPR, code2, op2a,
+						     op2b, code1, op1a, op1b))
+    return t;
+
+  return NULL_TREE;
+}
 
 /* Fold STMT to a constant using VALUEIZE to valueize SSA names.
 
